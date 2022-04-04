f44b96f617a894bfe4f819d83971a0fb15616317
pan93412 <pan93412@gmail.com>
2022-04-04 17:08:51 +0800

    refactor: canvas

--- canvas.js
+++ canvas.js
@@ -1,78 +1,120 @@
 /**
- * This js file controls the canvas.
- * @type {HTMLCanvasElement}
+ * This file controls the canvas.
  */
 
-// Get Canvas and inputs
-const canvas = document.querySelector("canvas");
-const context2d = canvas.getContext("2d");
+const DEFAULT_SEED = 1;
+const HEIGHT_RADIO = 1;
+const CURVE_NUMBER = 512;
 
-const defaultSeed = 1;
-let seed = defaultSeed;
-let canvasSrt = "NOTHING";
-
-const heightRatio = 1;
-canvas.height = canvas.width * heightRatio;
+/** @typedef {string} CSSSelector A valid CSS Selector. */
 
 /**
- * Paint the canvas using name and birthday as input.
+ * The possible SRT types.
+ * 
+ * @readonly
+ * @enum {string}
  */
-function paintCanvas(seedInput) {
-  seed = seedInput;
+const SRT_TYPES = {
+  Nothing: "NOTHING",
+  Generated: "GENERATED",
+};
 
-  let height = canvas.height;
-  let width = canvas.width;
+export class WormholeCanvas {
+  /**
+   * @param {CSSSelector=} [selector="canvas"]
+   */
+  constructor(selector = "canvas") {
+    this.canvas = document.querySelector(selector);
+    this.context2d = canvas.getContext("2d");
 
-  context2d.fillStyle = "black";
-  context2d.fillRect(0, 0, width, height);
+    this.seed = DEFAULT_SEED;
+    this.canvasSrt = SRT_TYPES.Nothing;
 
-  const curveNumber = 512;
-  for (let i = 0; i < curveNumber; i++) {
-    context2d.strokeStyle = `rgb(
-            ${Math.floor((255 * i) / curveNumber)},
-            ${Math.floor((255 * i) / curveNumber)},
-            ${Math.floor((255 * i) / curveNumber)})`;
-    context2d.beginPath();
-    context2d.moveTo(getRandomInt(width), getRandomInt(height));
-    context2d.bezierCurveTo(
-      width / 2 + getRandomInt(100) - 50,
-      height / 2 + getRandomInt(50) - 50,
-      width / 2 + getRandomInt(100) - 50,
-      height / 2 + getRandomInt(50) - 50,
-      getRandomInt(width),
-      getRandomInt(height)
-    );
-    context2d.stroke();
+    canvas.height = canvas.width * HEIGHT_RADIO;
   }
-  canvasSrt = "generated";
+
+  paint() {
+    this.statelessPaint();
+    this.canvasSrt = SRT_TYPES.Generated;
+  }
+
+  onClick() {
+    switch (canvasSrt) {
+      case SRT_TYPES.Generated:
+        playAudioThenRewind(`audio/a${getRandomInt(3)}.wav`);
+        break;
+    }
+  }
+
+  /** @private */
+  get height() {
+    return this.canvas.height;
+  }
+
+  /** @private */
+  get width() {
+    return this.canvas.width;
+  }
+
+  /**
+   * Paint the canvas using name and birthday as input.
+   * @private
+   */
+  statelessPaint() {
+    let height = this.height;
+    let width = this.width;
+  
+    context2d.fillStyle = "black";
+    context2d.fillRect(0, 0, width, height);
+  
+    for (let i = 0; i < CURVE_NUMBER; i++) {
+      context2d.strokeStyle = `rgb(
+              ${Math.floor((255 * i) / CURVE_NUMBER)},
+              ${Math.floor((255 * i) / CURVE_NUMBER)},
+              ${Math.floor((255 * i) / CURVE_NUMBER)})`;
+      context2d.beginPath();
+      context2d.moveTo(getRandomInt(width), getRandomInt(height));
+      context2d.bezierCurveTo(
+        width / 2 + getRandomInt(100) - 50,
+        height / 2 + getRandomInt(50) - 50,
+        width / 2 + getRandomInt(100) - 50,
+        height / 2 + getRandomInt(50) - 50,
+        getRandomInt(width),
+        getRandomInt(height)
+      );
+      context2d.stroke();
+    }
+  }
 }
 
 /**
- * Get a random int
- * @param max
+ * Get a random integer between min and max.
+ * 
+ * @param {number} max
+ * @param {number=} [min=0]
  * @returns {number}
  */
-function getRandomInt(max) {
-  return Math.floor(random() * max);
-}
+const getRandomInt = (max, min = 0) => (
+  Math.floor(getRandomBase() * max) + min
+);
 
-function random() {
+/**
+ * Generate a random number based on the global {@link seed}.
+ * 
+ * @returns {number}
+ */
+const getRandomBase = () => {
   let x = Math.sin(seed++) * 10000;
   return x - Math.floor(x);
-}
+};
 
-function canvasOnClick() {
-  switch (canvasSrt) {
-    case "NOTHING":
-      return;
-    case "generated":
-      let sound = new Audio("audio/a" + getRandomInt(3) + ".wav");
-      sound.play().then((r) => {
-        sound.currentTime = 0;
-      });
-      return;
-    default:
-      window.open(canvasSrt, "_blank");
-      return;
-  }
+/**
+ * Play the audio, and rewind it in the end.
+ * 
+ * @param {string} src The audio URI.
+ */
+const playAudioThenRewind = async (src) => {
+  const sound = new Audio(src);
+  await sound.play();
+  sound.currentTime = 0;
 }
